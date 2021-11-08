d2a95aae94c3979338459ee01037ad2e9f946d0e
Blargedy <berni.kleiner@gmail.com>
2021-11-08 03:15:10 -0700

    fixed variable names. added ms_per_mm

--- ActuatorController/ActuatorController.ino
+++ ActuatorController/ActuatorController.ino
@@ -1,30 +1,31 @@
+#include <Arduino.h>
+
+
 // constants won't change. Used here to set a pin number:
 const int ledPin =  LED_BUILTIN;// the number of the LED pin
 const int Forward_Pin = 1; //H bridge input 1 on pin D1
 const int Backward_Pin = 2; //H bridge input 2 on pin D2
-const int InterruptForward = 3; //manual button to go forward
-const int InterruptBackward = 4; //manual button to go backward
+const int ButtonForward = 3; //manual button to go forward
+const int ButtonBackward = 4; //manual button to go backward
 
 
-// Variables will change:
 bool homed =  false;
-
-unsigned long TimeMoved = 0;        // will store last time LED was updated
+unsigned long TimeMoved = 0;        // will store the elapsed time during a move
 
 // constants won't change:
-const long interval = 1500;           // interval at which to blink (milliseconds)
+const int ms_per_mm = 100;           // Time in ms to move actuator 1 mm (10mm/s)
 
 void setup() {
   // initialize mode of pins
   pinMode(ledPin, OUTPUT);
   pinMode(Forward_Pin, OUTPUT);
   pinMode(Backward_Pin, OUTPUT);
-  pinMode(InterruptBackward, INPUT);
-  pinMode(InterruptForward, INPUT);
+  pinMode(ButtonBackward, INPUT);
+  pinMode(ButtonForward, INPUT);
 
   //default pins to no movement
-  digitalWrite(FWD_Pin, LOW);
-  digitalWrite(BWD_Pin, LOW);
+  digitalWrite(Forward_Pin, LOW);
+  digitalWrite(Backward_Pin, LOW);
 }
 
 void loop() {
@@ -37,7 +38,7 @@ byte delayWhileCheckingPushButtons(unsigned int delayTime)
   for (unsigned int i = 0; i < delayTime; i += 10)
   {
     delay(10);
-    if (digitalRead(InterruptFoward) == HIGH || digitalRead(InterruptBackward) == HIGH)
+    if (digitalRead(Buttonforward) == HIGH || digitalRead(ButtonBackward) == HIGH)
       return(1);
   }
   return(0);
